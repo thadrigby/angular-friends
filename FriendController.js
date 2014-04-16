45081ab33e48f19bea6eb84e44191d0073f6704a
Thad Rigby <thadrigby@gmail.com>
2014-04-15 21:38:47 -0600

    uploaded friends, changed location, pics, status

--- FriendController.js
+++ FriendController.js
@@ -1,4 +1,13 @@
-
+angular.module('angular-friends').controller('friendController', function($scope) {
+	//everything happens here
+	$scope.test='hello world';
+	$.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function (myJsonDataFromServer) {
+		$scope.$apply(function() {
+			$scope.friends = myJsonDataFromServer.results;
+		});
+	
+	});
+});
 
 
 
